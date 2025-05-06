4468adf07aaced4b22a8427308642042216250c4
ArtByteFilip <artbytefilip@protonmail.com>
2025-05-06 23:04:49 +0200

    entry

---
+++ main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#include "mnamka.hpp"
+
+int main() {
+    std::cout << getMessage() << std::endl;
+    return 0;
+}
---
+++ mnamka.hpp
@@ -0,0 +1,11 @@
+#ifndef MNAMKA_H
+#define MNAMKA_H
+
+#include <string>
+
+std::string getMessage()
+{
+    return "Toto je správa z knižnice mnamka!";
+}
+
+#endif // MNAMKA_H
